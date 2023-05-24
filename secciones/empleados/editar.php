7246712af6de13cabc716ec419b1069ebfff2338
Jaaziel2345 <jaazielmc45@gmail.com>
2023-05-24 17:57:11 -0600

    correccion de error de subida de pdf e imagen.Funciona!

--- secciones/empleados/editar.php
+++ secciones/empleados/editar.php
@@ -8,8 +8,8 @@ if (isset($_GET['txtID'])) { //si recibimos ese dato
   //vamos a ocupar la instruccion prepare
   $sentencia = $conexion->prepare("SELECT * FROM tbl_empleados WHERE id=:id");
   //asignando los valores que tiene el metodo POST (los que vienen del formulario)
-  $sentencia->bindParam(":id", $txtID); 
-  $sentencia->execute(); 
+  $sentencia->bindParam(":id", $txtID);
+  $sentencia->execute();
   $registro = $sentencia->fetch(PDO::FETCH_LAZY);
 
 
@@ -31,16 +31,16 @@ if (isset($_GET['txtID'])) { //si recibimos ese dato
 }
 
 if ($_POST) {
- $txtID = (isset($_POST['txtID'])) ? $_POST['txtID'] : ""; // vamos a almacenar el id
- $primernombre=(isset($_POST["primernombre"])? $_POST["primernombre"]:"");
- $segundonombre=(isset($_POST["segundonombre"])? $_POST["segundonombre"]:"");
- $primerapellido=(isset($_POST["primerapellido"])? $_POST["primerapellido"]:""); 
- $segundoapellido=(isset($_POST["segundoapellido"])? $_POST["segundoapellido"]:"");
- $idpuesto=(isset($_POST["idpuesto"])? $_POST["idpuesto"]:"");
- $fechadeingreso=(isset($_POST["fechadeingreso"])? $_POST["fechadeingreso"]:"");   
-  
- //preparamos la insercion de los datos
- $sentencia = $conexion->prepare("
+  $txtID = (isset($_POST['txtID'])) ? $_POST['txtID'] : ""; // vamos a almacenar el id
+  $primernombre = (isset($_POST["primernombre"]) ? $_POST["primernombre"] : "");
+  $segundonombre = (isset($_POST["segundonombre"]) ? $_POST["segundonombre"] : "");
+  $primerapellido = (isset($_POST["primerapellido"]) ? $_POST["primerapellido"] : "");
+  $segundoapellido = (isset($_POST["segundoapellido"]) ? $_POST["segundoapellido"] : "");
+  $idpuesto = (isset($_POST["idpuesto"]) ? $_POST["idpuesto"] : "");
+  $fechadeingreso = (isset($_POST["fechadeingreso"]) ? $_POST["fechadeingreso"] : "");
+
+  //preparamos la insercion de los datos
+  $sentencia = $conexion->prepare("
  UPDATE tbl_empleados 
  SET
    primernombre = :primernombre,
@@ -55,7 +55,7 @@ if ($_POST) {
 
   //asignando los valores que tiene el metodo POST (los que vienen del formulario)
   $sentencia->bindParam(":primernombre", $primernombre);
-  $sentencia->bindParam(":segundonombre", $segundonombre); 
+  $sentencia->bindParam(":segundonombre", $segundonombre);
   $sentencia->bindParam(":primerapellido", $primerapellido);
   $sentencia->bindParam(":segundoapellido", $segundoapellido);
   $sentencia->bindParam(":idpuesto", $idpuesto);
@@ -63,11 +63,63 @@ if ($_POST) {
   $sentencia->bindParam(":id", $txtID); //nos servira para actualizar ese dato y al final tener recolectada esa informacion
   $sentencia->execute();
 
-  $foto=(isset($_FILES["foto"]['name'])?$_FILES["foto"]['name']:"");
-  $cv=(isset($_FILES["cv"]['name'])?$_FILES["cv"]['name']:"");
+  $foto = (isset($_FILES["foto"]['name']) ? $_FILES["foto"]['name'] : "");
 
-   //redireccionaremos al index
-   header("Location:index.php");
+  $fecha_ = new DateTime(); //obtenemos el tiempo
+
+  $nombreArchivo_foto = ($foto != '') ? $fecha_->getTimestamp() . "_" . $_FILES["foto"]['name'] : "";
+  $tmp_foto = $_FILES["foto"]['tmp_name']; //utilizar un archivo temporal 
+
+  if ($tmp_foto != '') {
+    move_uploaded_file($tmp_foto, "./" . $nombreArchivo_foto);
+    //Buscar el archivo realacionado con el empleado
+    $sentencia = $conexion->prepare("SELECT foto FROM tbl_empleados WHERE id=:id");
+    $sentencia->bindParam(":id", $txtID);
+    $sentencia->execute();
+    $registro_recuperado = $sentencia->fetch(PDO::FETCH_LAZY); //nos va a devolver un registro
+
+    if (isset($registro_recuperado["foto"]) && $registro_recuperado["foto"] != "") {
+      if (file_exists("./" . $registro_recuperado["foto"])) {
+        unlink("./" . $registro_recuperado["foto"]);
+
+      }
+    }
+
+    $sentencia = $conexion->prepare("UPDATE tbl_empleados SET foto = :foto WHERE id = :id");
+    $sentencia->bindParam(":foto", $nombreArchivo_foto);
+    $sentencia->bindParam(":id", $txtID);
+    $sentencia->execute();
+  }
+
+  $cv = (isset($_FILES["cv"]['name']) ? $_FILES["cv"]['name'] : "");
+  $nombreArchivo_cv = ($cv != '') ? $fecha_->getTimestamp() . "_" . $_FILES["cv"]['name'] : ""; //armar el nuevo nombre con el tiempo para que no se sobreescriba con otros
+  $tmp_cv = $_FILES["cv"]['tmp_name']; //utilizar un archivo temporal 
+
+  if ($tmp_cv != '') {
+    move_uploaded_file($tmp_cv, "./" . $nombreArchivo_cv); // para que podamos mover el archivo temporal a un nuevo destino
+
+    //Buscar el archivo realacionado con el empleado
+    $sentencia = $conexion->prepare("SELECT cv FROM tbl_empleados WHERE id=:id");
+    $sentencia->bindParam(":id", $txtID);
+    $sentencia->execute();
+    $registro_recuperado = $sentencia->fetch(PDO::FETCH_LAZY); //nos va a devolver un registro
+
+    if (isset($registro_recuperado["cv"]) && $registro_recuperado["cv"] != "") {
+      if (file_exists("./" . $registro_recuperado["cv"])) {
+        unlink("./" . $registro_recuperado["cv"]);
+
+      }
+    }
+
+    $sentencia = $conexion->prepare("UPDATE tbl_empleados SET cv=:cv WHERE id = :id");
+    $sentencia->bindParam(":cv", $nombreArchivo_cv);
+    $sentencia->bindParam(":id", $txtID);
+    $sentencia->execute();
+  }
+
+
+  //redireccionaremos al index
+  header("Location:index.php");
 }
 
 
@@ -84,54 +136,44 @@ if ($_POST) {
     <form action="" method="post" enctype="multipart/form-data">
       <!-- bs5form-input -->
       <div class="mb-3">
-              <label for="txtID" class="form-label">ID:</label>
-              <input type="text"
-              value="<?php echo $txtID;?>"
-                class="form-control" readonly name="txtID" id="txtID" aria-describedby="helpId" placeholder="ID">
-            </div>
+        <label for="txtID" class="form-label">ID:</label>
+        <input type="text" value="<?php echo $txtID; ?>" class="form-control" readonly name="txtID" id="txtID"
+          aria-describedby="helpId" placeholder="ID">
+      </div>
       <!-- bs5forminput -->
       <div class="mb-3">
         <label for="primernombre" class="form-label">Primer Nombre</label>
-        <input type="text" 
-        value="<?php echo $primernombre;?>"
-        class="form-control" name="primernombre" id="primernombre" aria-describedby="helpId"
-          placeholder="Ejemplo: Carlos...">
+        <input type="text" value="<?php echo $primernombre; ?>" class="form-control" name="primernombre"
+          id="primernombre" aria-describedby="helpId" placeholder="Ejemplo: Carlos...">
       </div>
       <div class="mb-3">
         <label for="segundonombre" class="form-label">Segundo Nombre</label>
-        <input type="text" 
-        value="<?php echo $segundonombre;?>"
-        class="form-control" name="segundonombre" id="segundonombre" aria-describedby="helpId"
-          placeholder="Ejemplo: Isaac...">
+        <input type="text" value="<?php echo $segundonombre; ?>" class="form-control" name="segundonombre"
+          id="segundonombre" aria-describedby="helpId" placeholder="Ejemplo: Isaac...">
       </div>
       <div class="mb-3">
         <label for="primerapellido" class="form-label">Primer Apellido</label>
-        <input type="text" 
-        value="<?php echo $primerapellido;?>"
-        class="form-control" name="primerapellido" id="primerapellido" aria-describedby="helpId"
-          placeholder="Ejemplo: Hernandez...">
+        <input type="text" value="<?php echo $primerapellido; ?>" class="form-control" name="primerapellido"
+          id="primerapellido" aria-describedby="helpId" placeholder="Ejemplo: Hernandez...">
       </div>
       <div class="mb-3">
         <label for="segundoapellido" class="form-label">Segundo Apellido</label>
-        <input type="text" 
-        value="<?php echo $segundoapellido;?>"
-        class="form-control" name="segundoapellido" id="segundoapellido" aria-describedby="helpId"
-          placeholder="Ejemplo: Cubias...">
+        <input type="text" value="<?php echo $segundoapellido; ?>" class="form-control" name="segundoapellido"
+          id="segundoapellido" aria-describedby="helpId" placeholder="Ejemplo: Cubias...">
       </div>
       <div class="mb-3">
         <label for="foto" class="form-label">Foto:</label>
-        <br/>
-            <img width="100" src="<?php echo $foto; ?>" class="rounded" alt="">
-        <br/>
-        <br/>
-        <input type="file" 
-        class="form-control" name="foto" id="foto" aria-describedby="helpId" placeholder="Foto.jpg">
+        <br />
+        <img width="100" src="<?php echo $foto; ?>" class="rounded" alt="">
+        <br />
+        <br />
+        <input type="file" class="form-control" name="foto" id="foto" aria-describedby="helpId" placeholder="Foto.jpg">
       </div>
 
       <div class="mb-3">
         <label for="cv" class="form-label">CV(PDF)</label>
-          <br>
-        <a href="<?php echo $cv;?>"><?php echo $cv;?></a>
+        <br>
+        <a href="<?php echo $cv; ?>"><?php echo $cv; ?></a>
         <input type="file" class="form-control" name="cv" id="cv" aria-describedby="helpId" placeholder="CV">
       </div>
 
@@ -139,11 +181,11 @@ if ($_POST) {
       <!-- bs5formselect-custom -->
       <div class="mb-3">
         <label for="idpuesto" class="form-label">Puesto</label>
-        <!-- "<?php echo $idpuesto;?>" -->
+        <!-- "<?php echo $idpuesto; ?>" -->
         <select class="form-select form-select-sm" name="idpuesto" id="idpuesto">
           <?php foreach ($lista_tbl_puestos as $registro) { ?>
 
-            <option <?php echo($idpuesto==$registro['id'])?"selected":"";?> value="<?php echo $registro['id'] ?>">
+            <option <?php echo ($idpuesto == $registro['id']) ? "selected" : ""; ?> value="<?php echo $registro['id'] ?>">
               <?php echo $registro['nombredelpuesto'] ?>
             </option>
 
@@ -155,10 +197,8 @@ if ($_POST) {
       <!-- bs5form-email -->
       <div class="mb-3">
         <label for="fechadeingreso" class="form-label">Fecha de ingreso:</label>
-        <input type="date"
-        value="<?php echo $fechadeingreso;?>"
-        class="form-control" name="fechadeingreso" id="fechadeingreso" aria-describedby="emailHelpId"
-          placeholder="">
+        <input type="date" value="<?php echo $fechadeingreso; ?>" class="form-control" name="fechadeingreso"
+          id="fechadeingreso" aria-describedby="emailHelpId" placeholder="">
       </div>
 
       <!-- bs5button-default -->
